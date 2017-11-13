96eab1e6588508db0f6ad21c3d2be649b44659ce
Ville Kautonen <ville.kautonen@metropolia.fi>
2017-11-13 09:39:49 +0000

    14 Modular PHP exercises complete

--- labs/14 Modular PHP/diceclasses.inc.php
+++ labs/14 Modular PHP/diceclasses.inc.php
@@ -1,16 +1,26 @@
 <?php
 
 class Dice {
-    private  $faces;
-    private  $freqs = array();
+    protected  $faces;
+    protected  $bias;
+    protected  $freqs = array();
     
     // Constructor
-    public function __construct($faces) {
+    public function __construct($faces, $bias) {
         $this->faces = $faces;
+        if ($bias) {
+            $this->bias = $bias;
+        } else {
+            $this->bias = 1/$this->faces;
+        }
     }
     
     public function cast() {
-        $res = rand(1,$this->faces);
+        if ((rand(0,100)-$this->bias*100)<=0) {
+            $res = $this->faces;
+        } else {
+            $res = rand(1,$this->faces-1);
+        }
         $this->freqs[$res]++;
         return $res;
     }
@@ -20,6 +30,24 @@ class Dice {
         if ($freq=="")
             $freq = 0;
         return $freq;
+    }
+    
+    public function getAvgEyes(array $results) {
+        $sum = 0;
+        foreach ($results as $value) {
+            $sum += $value["res"];
+        }
+        $avg = $sum/sizeof($results);
+        return $avg;
+    }
+}
+
+class PhysicalDice extends Dice {
+    protected $material;
+    
+    function __construct($faces, $bias, $material) {
+        parent::__construct($faces, $bias);
+        $this->material = $material;
     }
 }
 
--- labs/14 Modular PHP/diceplay.php
+++ labs/14 Modular PHP/diceplay.php
@@ -4,11 +4,17 @@ include("diceclasses.inc.php");
 
 $faces = $_GET["faces"];
 $throws = $_GET["throws"];
+$material = $_GET["material"];
+$bias = $_GET["bias"];
 
 $results = array();
 
 // make dice
-$dice = new Dice($faces);
+if ($material) {
+    $dice = new PhysicalDice($faces, $bias, $material);
+} else {
+    $dice = new Dice($faces, $bias);
+}
 for ($i = 1; $i<=$throws; $i++) {
     $res = $dice->cast();
     $results[] = array('id' => strval($i), 'res' => strval($res));
@@ -17,7 +23,8 @@ $freqs = array();
 for ($i = 1; $i<=$faces; $i++) {
     $freqs[] = array ('eyes' => strval($i), 'frequency' => strval($dice->getFreq($i)));
 }
-echo json_encode(array('faces'=>$faces,'results'=>$results,'frequencies'=>$freqs));
+$aveg = $dice->getAvgEyes($results);
+echo json_encode(array('faces'=>$faces,'results'=>$results,'frequencies'=>$freqs,'average'=>$aveg,'material'=>$material));
 
 
 
